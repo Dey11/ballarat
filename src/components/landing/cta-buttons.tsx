dcdbbb6aabc7ea1fc62c00bea4c59bf0609514a6
Dey11 <shreyandey11@gmail.com>
2025-09-29 21:27:29 +0530

    fix layout shift

--- src/components/landing/cta-buttons.tsx
+++ src/components/landing/cta-buttons.tsx
@@ -10,7 +10,7 @@ export default function CTAButtons() {
       <motion.button
         initial="initial"
         whileHover="hover"
-        className="font-roboto-condensed group bg-primary text-foreground hover:border-primary rounded-brand hover:bg-foreground hover:text-primary flex cursor-pointer items-center gap-x-2 px-4 py-2 text-lg font-semibold whitespace-nowrap hover:border-b-2 md:py-1"
+        className="font-roboto-condensed group bg-primary text-foreground hover:border-primary rounded-brand hover:bg-foreground hover:text-primary flex cursor-pointer items-center gap-x-2 border-b-2 border-b-transparent px-4 py-2 text-lg font-semibold whitespace-nowrap hover:border-b-2 md:py-1"
       >
         <span>Register Interest</span>
         <div className="bg-background group-hover:bg-primary group-hover:text-foreground p-0.2 group-hover:p-0.1 relative flex items-center justify-center overflow-hidden rounded-xs group-hover:rounded-full">
